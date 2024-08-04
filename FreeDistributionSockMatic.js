e7f0ad5a9e58388c5187a5d6d077fd482f2e6fcc
detogo <detogo.app@gmail.com>
2024-08-04 16:32:13 -0600

    free distribution contrac verifyed 7

--- FreeDistributionSockMatic.js
+++ FreeDistributionSockMatic.js
@@ -55,35 +55,23 @@ async function sendETHtowallet(){
 	    
 	    const walletelement = document.getElementById('text-address-to-send-ETH');      
 	    const friendAddress=walletelement.value;
-
-	    
-
+    
 	    const balanceEther = await web3.eth.getBalance(friendAddress); 
 	    
 	    if (balanceEther <= web3.utils.toWei('0.0', 'ether')) {
 	          try{
 	          	 alert('friendAddress  '+ friendAddress);
 	             const transaction = await contractFREETOKENS.methods.sendEtherToAddress(friendAddress).send({from: myAddress, gas: 300000, gasPrice: web3.utils.toWei('50', 'gwei') });      
-	                                
-                 
+                                                 
 	          }catch(error){
 	               console.error('Error al realizar la transacción:', error);
 	               alert('Error al realizar la transacción: ' + error.message);
-
 	          }
 
-
 	    }else{
-	      alert('El usuario ya posee ETHER');
-
-	      console.log('El usuario tiene Ether.');
+		      alert('El usuario ya posee ETHER');
+		      console.log('El usuario tiene Ether.');
 	    }
 
 	    walletelement.innerHTML = '';
-
-}
-
-
-
-
- 
+} 

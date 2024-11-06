84d2fc843c35c61514a2327082146fd87a067dcf
detogo <detogo.app@gmail.com>
2024-11-06 00:22:19 -0600

    wallet

--- main.js
+++ main.js
@@ -2,5 +2,8 @@
 import { ethers } from "./ethers.js-main/dist/ethers.min.js";
 
 // Código para la configuración del proveedor
+let globalWalletKey = "";
+
+const provider = new ethers.providers.JsonRpcProvider("https://polygon-mainnet.g.alchemy.com/v2/8gJweGU1u8NB60FICShTvPFy3oUu_zsA");
 
 // Otros códigos que quieras incluir...

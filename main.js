8a0230400d6d2610c2b49fdea496605b583c845e
detogo <detogo.app@gmail.com>
2024-11-06 00:10:08 -0600

    wallet

---
+++ main.js
@@ -0,0 +1,6 @@
+// Importa ethers desde la ruta correcta
+import { ethers } from "./ethers.js-main/dist/ethers.min.js";
+
+// Código para la configuración del proveedor
+
+// Otros códigos que quieras incluir...

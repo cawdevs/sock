7f546edfd337f269165479a7e80949d721647e16
detogo <detogo.app@gmail.com>
2024-12-30 00:35:00 -0600

    bet transactions

--- games/sockWar/scriptSockWar.js
+++ games/sockWar/scriptSockWar.js
@@ -256,6 +256,7 @@ async function  approve_Bet_SockWar() {
             //bet_Amount = await betContract.betAmount(); 
             console.log(" bet_Amount SOCK: ",  bet_Amount);
             amountToApprove = ethers.utils.parseUnits(bet_Amount, 'ether');  // Convertir a ethers usando la cantidad de decimales correcta
+            amountToApprove = amountToApprove.toString();
             //amountToApprove = betAmount;
             console.log(" amountToApproveSOCK: ",  amountToApprove.toString());
             // Aprobar el gasto usando ethers.js

ed789becabbdd06ba1510f4e69f4441c9d7762ee
cawdevs <detogo.app@gmail.com>
2026-09-20 14:14:35 -0600

    avatar nuevo

--- postPublication.js
+++ postPublication.js
@@ -1045,9 +1045,9 @@ async function get_publication(id_publication,principalContainerID) {
                         }
 
 
-                         //await loadImagesFromHex(codeHexaImage, profileImageContainerId, "small");
-                         const div = document.getElementById(`imageContainerId_${publicationObject.id}`);
-                         generarAvatar_desde_codigo(codeHexaImage,div,"redondo",50,50,100,100,0);
+                         await loadImagesFromHex(codeHexaImage, profileImageContainerId, "small");
+                         //const div = document.getElementById(`imageContainerId_${publicationObject.id}`);
+                         //generarAvatar_desde_codigo(codeHexaImage,div,"redondo",50,50,100,100,0);
                 }                      
                          
                         
